c97a01f0a7df4293d662e2bc4a6c626727f5118c
katarzyna <katarzyna@iMac-katarzyna.home>
2017-02-08 23:03:25 +0100

    First attempt to canvas grill

---
+++ grill/grill.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var background_canvas = document.getElementById("main");
+  background_canvas.width = window.innerWidth;
+  background_canvas.height = window.innerHeight;
+  var context = main.getContext("2d");
+}, false);
